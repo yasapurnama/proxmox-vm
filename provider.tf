a36e83d31bebf425ddb4fc969827fa2b6ef76437
Purname Yasa <yasapurnama@gmail.com>
2024-08-30 09:36:53 +0800

    init

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "proxmox" {
+  pm_api_url = var.pm_api_url
+  pm_user = var.pm_user
+  pm_password = var.pm_password
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "pm_api_url" {
+  description = "The URL of the Proxmox API"
+  type        = string
+}
+
+variable "pm_user" {
+  description = "The username to authenticate with the Proxmox API"
+  type        = string
+}
+
+variable "pm_password" {
+  description = "The password to authenticate with the Proxmox API"
+  type        = string
+}
